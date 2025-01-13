3412a4065143bceba7492f7709f9f88bd579e59d
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2025-01-13 19:25:30 +0000

    Created collectPayment, landingPage, sendInvoice and LICENSE.txt; Edited settings; Deleted Form1

---
+++ client_code/landingPage/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import landingPageTemplate
+from anvil import *
+
+
+class landingPage(landingPageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def create_invoice_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def collect_payment_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
