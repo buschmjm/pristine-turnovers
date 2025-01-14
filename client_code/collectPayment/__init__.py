6661ba6c020a9676b6994084d9af7dcdcbaf4924
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2025-01-14 16:56:00 +0000

    Edited collectPayment and settings

--- client_code/collectPayment/__init__.py
+++ client_code/collectPayment/__init__.py
@@ -8,12 +8,16 @@ import anvil.users
 
 
 class collectPayment(collectPaymentTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    # Any code you write here will run before the form opens.   
-  
-  def customer_selector_click()(self, **event_args):
-    self.fetch_customers()
-    
+    def __init__(self, **properties):
+      # Set Form properties and Data Bindings.
+      self.init_components(**properties)
+      # Any code you write here will run before the form opens.   
+
+    def customer_selector_show(self, **event_args):
+      try:
+        customers = anvil.server.call('get_recent_customers')
+        self.customer_selector.items = customers
+      except anvil.server.AnvilWrappedError as e:
+        alert(f"Failed to retrieve Customers: {e}")
+      pass
   
