5865f9c0d1d834aad3a5854bef4f9bf8ddbf7499
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2025-01-16 21:45:47 +0000

    Created billingLibrary.RowTemplate1; Edited landingPage and billingLibrary

--- client_code/landingPage/__init__.py
+++ client_code/landingPage/__init__.py
@@ -16,10 +16,10 @@ class landingPage(landingPageTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def create_invoice_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
-
   def collect_payment_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     anvil.open_form('collectPayment')
+
+  def billing_library_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.open_form('billingLibrary')
