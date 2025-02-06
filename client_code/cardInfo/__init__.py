44531d450a21a6cb5cf41647f1c5bf042abfe77e
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2025-02-06 16:19:33 +0000

    Edited cardInfo

--- client_code/cardInfo/__init__.py
+++ client_code/cardInfo/__init__.py
@@ -13,3 +13,7 @@ class cardInfo(cardInfoTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def process_payment_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
